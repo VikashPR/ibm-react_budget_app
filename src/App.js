1aca6d1fa1f24dcbd20f2cc22fcfd1c358f44a74
VikashPR <vikashrajendran2806@gmail.com>
2023-08-14 18:57:45 +0530

    feat: budget component add

--- src/App.js
+++ src/App.js
@@ -11,25 +11,22 @@ import Budget from './components/Budget';
 import { AppProvider } from './context/AppContext';
 const App = () => {
     return (
-        <AppProvider>
-            <div className='container'>
-                <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
-                        {/* Add Budget component here under */}        
-
-                        {/* Add Remaining component here under */}        
-
-                        {/* Add ExpenseTotal component here under */}        
-                       
-                        {/* Add ExpenseList component here under */}         
-
-                        {/* Add ExpenseItem component here under */}        
-
-                        {/* Add AllocationForm component here under */}        
-
-                </div>
+      <AppProvider>
+        <div className="container">
+          <h1 className="mt-3">Company's Budget Allocation</h1>
+          <div className="row mt-3">
+                {/* Add Budget component here under */}
+            <div className="col-sm">
+              <Budget />
             </div>
-        </AppProvider>
+                {/* Add Remaining component here under */}   {" "}
+            {/* Add ExpenseTotal component here under */}   {" "}
+            {/* Add ExpenseList component here under */}   {" "}
+            {/* Add ExpenseItem component here under */}   {" "}
+            {/* Add AllocationForm component here under */}
+          </div>
+        </div>
+      </AppProvider>
     );
 };
 export default App;
